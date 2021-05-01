a1d5499fb70b2bcdb15eb65831405cda325b3850
HAUDRAUFHAUN <dietrich.poensgen@heg-portal.de>
2021-05-01 22:22:31 +0200

    Initial Commit

---
+++ AppCleaner.io/AppCleaner_ioApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCleaner_ioApp.swift
+//  AppCleaner.io
+//
+//  Created by Dietrich Poensgen on 01.05.21.
+//
+
+import SwiftUI
+
+@main
+struct AppCleaner_ioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
